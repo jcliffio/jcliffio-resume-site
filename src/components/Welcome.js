232e7d9b3b34c721c817883ae2ce3d29c0a97afe
Josh Clifford <josh@webtheory.io>
2015-05-08 09:24:12 -0500

    Fixed some typos in about text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -45,8 +45,8 @@ var Welcome = React.createClass({
             I thoroughly enjoy programming, listening to music (if it&#39;s not playing from my speakers, it&#39;s playing in my head), and spending time with my wife and family.
           </p>
           <p>
-            I graduated from <ExternalLink url="http://www.murraystate.edu" text="Murray State University" /> in 2013 with a Bachelor&#39;s Degree in Computer ience.
-            While at Murray, I tried to keep my education diverse by experimenting in different technologies, namely .NET, Android, Arduino.
+            I graduated from <ExternalLink url="http://www.murraystate.edu" text="Murray State University" /> in 2013 with a Bachelor&#39;s Degree in Computer Science.
+            While at Murray, I tried to keep my education diverse by experimenting in different technologies, namely .NET, Android, and Arduino.
           </p>
           <p>
             Post-college, I have narrowed my focus to primarily web development. I love all aspects of it, both front end and back end. From CSS to APIs, webdev is such a vibrant and varied domain, I find it next to impossible to get bored with it.
